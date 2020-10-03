b3916a066f69ef14fb06ff7302925fefc650684c
Lai Van On Hitu <laivanon.hitu@gmail.com>
2020-10-03 17:48:30 +0700

    JavaConfiguration Project

---
+++ JavaConfiguration/src/main/java/com/laivanon/config/SpringConfigureration.java
@@ -0,0 +1,23 @@
+package com.laivanon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.laivanon.controller")
+public class SpringConfigureration {
+	@Bean(name = "jspViewResolver")
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
